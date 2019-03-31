0fed3495b475a517d96488d6eea4e548d6b59274
Tarbadev <tarbadev@gmail.com>
2019-03-31 15:48:49 -0700

    Fix ci config for android emulator

--- ci/android/startEmulator.sh
+++ ci/android/startEmulator.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 
-echo no | android create avd --force -n test -k "system-images;android-28;google_apis;x86"
+echo no | avdmanager create avd --force -n test -k "system-images;android-28;google_apis;x86"
 emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
