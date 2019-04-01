504f608bb77653998c7fd09af9b5dcba5ea71c5e
Tarbadev <tarbadev@gmail.com>
2019-03-31 18:27:53 -0700

    Test CI config

--- ci/android/startEmulator.sh
+++ ci/android/startEmulator.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 
-echo no | android create avd --force -n test -t android-28
+echo no | android create avd --force -n test -t android-26
 emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
