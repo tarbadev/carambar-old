fcd3b6a8d65dca44f578d318020f8e358960c8d9
Tarbadev <tarbadev@gmail.com>
2019-03-31 19:15:48 -0700

    Test CI config

--- ci/ios/setupEnvironment.sh
+++ ci/ios/setupEnvironment.sh
@@ -2,13 +2,14 @@
 set -e
 set -x
 
-pip install six
+#pip install six
 brew update
 brew install --HEAD usbmuxd
-brew unlink usbmuxd
+#brew unlink usbmuxd
 brew link usbmuxd
 brew install --HEAD libimobiledevice
-brew install ideviceinstaller ios-deploy
+brew install ideviceinstaller ios-deploy cocoapods
+pod setup
 
 xcrun simctl list
 open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/
