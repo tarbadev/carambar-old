36bc7baddf8d14f1fbadc9f3f9d5dc4198742778
Tarbadev <tarbadev@gmail.com>
2019-03-31 21:34:37 -0700

    Test CI config

--- ci/android/startEmulator.sh
+++ ci/android/startEmulator.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 
-echo no | android create avd --force -n test -t android-25 --abi google_apis/armeabi-v7a
-emulator -avd test -no-window &
+echo no | android create avd --force -n test -t android-16 --abi google_apis/armeabi-v7a
+emulator -avd test -no-window --no-audio &
 android-wait-for-emulator
--- ci/exportBuildVersion.sh
+++ ci/exportBuildVersion.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+git clone --branch version --depth 1 https://github.com/tarbadev/carambar.git
 git clone --depth 1 https://github.com/fsaintjacques/semver-tool /tmp/semver &> /dev/null
-lastVersion=`git show origin/version:latest.txt`
+lastVersion=`cat version/latest.txt`
 export VERSION=`/tmp/semver/src/semver bump build ${TRAVIS_BUILD_NUMBER} ${lastVersion}`
