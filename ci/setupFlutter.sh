ce44503d9b8bfa5509416af3b7eeac40b4ce92cb
Tarbadev <tarbadev@gmail.com>
2019-12-22 20:36:03 -0800

    Test CI config

--- ci/setupFlutter.sh
+++ ci/setupFlutter.sh
@@ -3,4 +3,6 @@ set -e
 set -x
 
 git clone --branch v1.12.13+hotfix.5 --depth 1 https://github.com/flutter/flutter.git
-./flutter/bin/flutter doctor
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+export PATH=$PWD/flutter/bin:$PWD/flutter/bin/cache/dart-sdk/bin:$PATH
+flutter doctor -v
